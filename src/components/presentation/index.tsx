bb35c5345c8393e38b18d5cb34214aa269c4b5fe
Gustavo Soares <dev.gustavosoares@gmail.com>
2022-04-02 13:37:54 -0300

    Removido a classe 'content' que tinha como função clear:both, pelos testes a aplicação continua normal

--- src/components/presentation/index.tsx
+++ src/components/presentation/index.tsx
@@ -3,19 +3,18 @@ import styles from "./styles.module.scss";
 export function Presentation() {
     return (
         <section className={styles.contentLeft}>
-            <div className={styles.content}>
-                <div>
-                    <img src="/images/logo.svg" alt="Qfinances logo" />
-                </div>
+            <div>
+                <img src="/images/logo.svg" alt="Qfinances logo" />
+            </div>
 
-                <div>
-                    <h1>Seu dinheiro, seu controle!</h1>
-                    <p>
-                        Controle seus limites de gastos através do nosso app
-                        Para dados mais precisos obtenha o plano premium na
-                        nossa página na web
-                    </p>
-                </div>
+            <div>
+                <h1>Seu dinheiro, seu controle!</h1>
+                <p>
+                    Controle seus limites de gastos através do nosso app Para
+                    dados mais precisos obtenha o plano premium na nossa página
+                    na web
+                </p>
+
                 <div>
                     <img
                         id={styles.cards}
